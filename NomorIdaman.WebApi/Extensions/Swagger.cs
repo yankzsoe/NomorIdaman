8c556056f897496c4318643ad76d7301e5648267
yankzsoe <yankzsoe@gmail.com>
2022-03-08 22:14:47 +0700

    Add custom swagger

---
+++ NomorIdaman.WebApi/Extensions/Swagger.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace NomorIdaman.WebApi.Extensions {
+    public static class Swagger {
+        public static void AddSwagger(this IServiceCollection services) {
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Nomor Idaman API",
+                    Version = "v1",
+                    Description = "REST API for Nomor Idaman application"
+                }); ;
+                c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    BearerFormat = "JWT",
+                    Description = "Input your token in this format - Bearer {access token} to access this API",
+                });
+                c.OperationFilter<AuthResponsesOperationFilter>();
+                c.TagActionsBy(api => {
+                    if (api.GroupName != null) {
+                        return new[] { api.GroupName };
+                    }
+
+                    var controllerActionDescriptor = api.ActionDescriptor as ControllerActionDescriptor;
+                    if (controllerActionDescriptor != null) {
+                        return new[] { controllerActionDescriptor.ControllerName };
+                    }
+
+                    throw new InvalidOperationException("Unable to determine tag for endpoint.");
+                });
+                c.DocInclusionPredicate((name, api) => true);
+                c.IncludeXmlComments(string.Format(@"{0}\NomorIdaman.WebAPI.xml", AppDomain.CurrentDomain.BaseDirectory));
+                c.IncludeXmlComments(string.Format(@"{0}\NomorIdaman.Application.xml", AppDomain.CurrentDomain.BaseDirectory));
+                c.DescribeAllParametersInCamelCase();
+            });
+        }
+
+        public class AuthResponsesOperationFilter : IOperationFilter {
+            public void Apply(OpenApiOperation operation, OperationFilterContext context) {
+                var actionMetadata = context.ApiDescription.ActionDescriptor.EndpointMetadata;
+                var isAuthorized = actionMetadata.Any(metadataItem => metadataItem is AuthorizeAttribute);
+                var allowAnonymous = actionMetadata.Any(metadataItem => metadataItem is AllowAnonymousAttribute);
+
+                if (!isAuthorized || allowAnonymous) return;
+                if (operation.Parameters == null) operation.Parameters = new List<OpenApiParameter>();
+
+                operation.Security = new List<OpenApiSecurityRequirement> {
+                    new OpenApiSecurityRequirement {
+                        {
+                            new OpenApiSecurityScheme {
+                                Reference = new OpenApiReference {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = JwtBearerDefaults.AuthenticationScheme,
+                                },
+                                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                                Name = JwtBearerDefaults.AuthenticationScheme,
+                                In = ParameterLocation.Header,
+                            },
+                            new List<string>()
+                        },
+                    }
+                };
+            }
+        }
+
+        public static void UseSwaggerExtension(this IApplicationBuilder app) {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nomor Idaman API v1");
+                c.DefaultModelsExpandDepth(-1);
+            });
+        }
+    }
+}
--- NomorIdaman.WebApi/Program.cs
+++ NomorIdaman.WebApi/Program.cs
@@ -1,5 +1,7 @@
 using NomorIdaman.Application;
 using NomorIdaman.Infrastructure;
+using NomorIdaman.WebApi.Extensions;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,25 +10,23 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 
 //  INTERNAL SERVICES
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+    .AddJsonOptions(option => option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerExtension();
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
