e5ea1f7e20c2022cf4dbdd72d75d5520b0e3b73c
yankzsoe <yankzsoe@gmail.com>
2022-03-08 22:16:01 +0700

    add MediatR and FluentValidation

--- NomorIdaman.Application/ServiceExtentions.cs
+++ NomorIdaman.Application/ServiceExtentions.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace NomorIdaman.Application {
     public static class ServiceExtentions {
         public static void AddApplication(this IServiceCollection services, IConfiguration configuration) {
-
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
     }
 }
