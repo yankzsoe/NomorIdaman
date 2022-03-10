2d4a6521280cf34c513c9d0d520c8d327bcde8a1
yankzsoe <yankzsoe@gmail.com>
2022-03-10 22:00:43 +0700

    initialize index

--- NomorIdaman/Controllers/HomeController.cs
+++ NomorIdaman/Controllers/HomeController.cs
@@ -1,31 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NomorIdaman.Models;
+using NomorIdaman.WebApplication.Interface;
+using NomorIdaman.WebApplication.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
-namespace NomorIdaman.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace NomorIdaman.WebApplication.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBridgeApiSettings _bridgeApiSettings;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger, IBridgeApiSettings bridgeApiSettings) {
             _logger = logger;
+            _bridgeApiSettings = bridgeApiSettings;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index() {
+            var vm = new IndexViewModel();
+            using (var httpClient = new HttpClient()) {
+                httpClient.BaseAddress = new Uri(_bridgeApiSettings.BaseAPiUrl);
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                Task<HttpResponseMessage> simCardResponse = httpClient.GetAsync("api/sim-card?keyword=&isActive=true&sortBy=Asc&orderBy=Shop&pageNumber=1&pageSize=10");
+                JsonNode simCardNode = JsonNode.Parse(simCardResponse.Result.Content.ReadAsStringAsync().Result.ToString());
+                if (simCardNode["succeeded"].GetValue<bool>()) {
+                    var simCards = new List<SIMCardViewModel>();
+                    JsonNode data = simCardNode["data"];
+                    var count = data.AsArray().Count;
+                    for (int i = 0; i < count; i++) {
+                        var item = new SIMCardViewModel() {
+                            ProviderCardId = data[i]["providerCardId"].GetValue<int>(),
+                            ProviderCardName = data[i]["providerCardName"].GetValue<string>(),
+                            ShopCode = data[i]["shopCode"].GetValue<string>(),
+                            ShopId = data[i]["shopId"].GetValue<int>(),
+                            CardNumber = data[i]["cardNumber"].GetValue<string>(),
+                            Price = data[i]["price"].GetValue<decimal>(),
+                        };
+                        simCards.Add(item);
+                    }
+                    vm.simCards = simCards;
+                }
+
+                Task<HttpResponseMessage> providerResponse = httpClient.GetAsync("api/provider-card");
+                JsonNode providerNode = JsonNode.Parse(providerResponse.Result.Content.ReadAsStringAsync().Result.ToString());
+                if (providerNode["succeeded"].GetValue<bool>()) {
+                    var providers = new List<ProviderViewModel>();
+                    JsonNode data = providerNode["data"];
+                    var count = data.AsArray().Count;
+                    for (int i = 0; i < count; i++) {
+                        var item = new ProviderViewModel() {
+                            Id = data[i]["id"].GetValue<int>(),
+                            Name = data[i]["name"].GetValue<string>()
+                        };
+                        providers.Add(item);
+                    }
+                    vm.providers = providers;
+                }
+
+                Task<HttpResponseMessage> shopResponse = httpClient.GetAsync("api/shop");
+                JsonNode shopNode = JsonNode.Parse(shopResponse.Result.Content.ReadAsStringAsync().Result.ToString());
+                if (shopNode["succeeded"].GetValue<bool>()) {
+                    var shops = new List<ShopViewModel>();
+                    JsonNode data = shopNode["data"];
+                    var count = data.AsArray().Count;
+                    for (int i = 0; i < count; i++) {
+                        var item = new ShopViewModel() {
+                            Code = data[i]["code"].GetValue<string>(),
+                            Name = data[i]["name"].GetValue<string>()
+                        };
+                        shops.Add(item);
+                    }
+                    vm.shops = shops;
+                }
+            }
+
+            return View(vm);
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
---
+++ NomorIdaman/Models/IndexViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace NomorIdaman.WebApplication.Models {
+    public class IndexViewModel {
+        public string ShopSelected { get; set; }
+        public string ProviderSelected { get; set; }
+        public string SIMCardSelected { get; set; }
+
+        public IEnumerable<ProviderViewModel> providers { get; set; }
+        public IEnumerable<ShopViewModel> shops { get; set; }
+        public IEnumerable<SIMCardViewModel> simCards { get; set; }
+    }
+
+    public class ProviderViewModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ShopViewModel {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SIMCardViewModel {
+        public string CardNumber { get; set; }
+        public decimal Price { get; set; }
+        public int ProviderCardId { get; set; }
+        public string ProviderCardName { get; set; }
+        public string ShopCode { get; set; }
+        public int ShopId { get; set; }
+    }
+}
